b7ad86d24f3eb6a0d4112c85c0c9858e048de15f
smirniy <89267095924@mail.ru>
2024-09-17 20:51:29 +0300

    12.05.24_19:13

--- pythonProject1/hallo.py
+++ pythonProject1/hallo.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, session, url_for, redirect
-import openpyxl
+from baza import *
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'hard to guess string'  # настраиваем ключ шифрования
 
 choice = ['Нет, не применять', 'Да, применить']
+cat = ['1', '2', '3']
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -38,9 +39,6 @@ def index():
                                choice=choice)
 
 
-cat = ['1', '2', '3']
-
-
 @app.route('/smeta', methods=['POST', 'GET'])
 def smeta():
     if request.method == 'POST':
@@ -50,12 +48,13 @@ def smeta():
         floor = request.form.get('floor')  # этажность (одно-/много-этажное)
         category_1 = request.form.get('category_1')  # категория сложности здания
         category_2 = request.form.get('category_2')  # сложность обмерных работ
-        category_3 = request.form.get('category_3')
+        category_3 = request.form.get('category_3')  # сложность обследовательских работ
 
         k_2 = request.form.get('k_2')  # Шаг колонн и несущих стен менее 6 м. (п.2.1.9) = 1.25
         k_3 = request.form.get('k_3')  # Обмеры и обследование клёпаных конструкций (п.2.1.10) = 1.2
         k_4 = request.form.get('k_4')  # Выполнение обмерных работ с использованием имеющихся чертежей (п.2.1.12) = 0.75
-        k_5 = request.form.get('k_5')  # выполнение поверочного расчета строительных конструкций не требуется (п.2.1.13) = 0.8
+        k_5 = request.form.get('k_5')
+        # Выполнение поверочного расчета строительных конструкций не требуется (п.2.1.13) = 0.8
 
         k10_1 = request.form.get('k10_1')  # = 1.2
         k10_2 = request.form.get('k10_2')  # = 1.15
@@ -73,8 +72,8 @@ def smeta():
         k10_14 = request.form.get('k10_14')  # = 1.25
 
         k1 = 0.8 if purpose == '2' else 1  # если промышленное
-        k2 = 1.25 if k_2 else 1
-        k3 = 1.2 if k_3 else 1
+        k2 = 1.25 if k_2 else 1  # Шаг колонн
+        k3 = 1.2 if k_3 else 1  # Клёпаные конструкции
         k4 = 0.75 if k_4 else 1  # Только для обмерных работ!
         k5 = 0.8 if k_5 else 1  # Только для обследовательских работ!
 
@@ -98,47 +97,27 @@ def smeta():
 
         k10 = k101 * k102 * k103 * k104 * k105 * k106 * k107 * k108 * k109 * k1010 * k1011 * k1012 * k1013 * k1014
 
-        book = openpyxl.open('data.xlsx', read_only=True)  # открыли файл только для чтения
-        sheet = book.active  # позиционируемся на 1-ом листе
-        i = 2
-
-        tab_1 = {(1, 1): 7, (1, 2): 8, (1, 3): 9,
-                 (2, 1): 11, (2, 2): 12, (2, 3): 13,
-                 (3, 1): 15, (3, 2): 16, (3, 3): 17}
-
-        tab_2 = {(1, 1): 26, (1, 2): 27, (1, 3): 28,
-                 (2, 1): 30, (2, 2): 31, (2, 3): 32,
-                 (3, 1): 34, (3, 2): 35, (3, 3): 36}
-
-        tab_3 = {(1, 1): 45, (1, 2): 46, (1, 3): 47,
-                 (2, 1): 49, (2, 2): 50, (2, 3): 51,
-                 (3, 1): 53, (3, 2): 54, (3, 3): 55}
-
-        tab_4 = {(1, 1): 64, (1, 2): 65, (1, 3): 66,
-                 (2, 1): 68, (2, 2): 69, (2, 3): 70,
-                 (3, 1): 72, (3, 2): 73, (3, 3): 74}
+        if int(height) > 21:
+            height = 21
 
         if floor == '1':
-            k = tab_1[int(category_1), int(category_2)]
-            j = tab_3[int(category_1), int(category_3)]
+            if int(height) < 4:
+                height = 4
+            cost_obm = tab1[int(category_1) - 1][int(category_2) - 1][int(height) - 4]
+            cost_obsl = tab3[int(category_1) - 1][int(category_3) - 1][int(height) - 4]
+
         else:
-            k = tab_2[int(category_1), int(category_2)]
-            j = tab_4[int(category_1), int(category_3)]
+            if int(height) < 6:
+                height = 6
+            cost_obm = tab2[int(category_1) - 1][int(category_2) - 1][int(height) - 6]
+            cost_obsl = tab4[int(category_1) - 1][int(category_3) - 1][int(height) - 6]
 
-        while int(height) != int(sheet[4][i].value):
-            i += 1
-
-        cost_obm = sheet[k][i].value
-        cost_obsl = sheet[j][i].value
-
-        K_inf = 4.91  # II кв. 2022 (http://www.consultant.ru/document/cons_doc_LAW_39473/)
+        K_inf = 5.96  # II кв. 2024 (https://www.consultant.ru/document/cons_doc_LAW_39473/)
 
         results_1 = round(int(volume) * 0.01 * cost_obm * K_inf * k_obm * k10, 2)  # стоимость обмерных работ
         results_2 = round(int(volume) * 0.01 * cost_obsl * K_inf * k_obs * k10, 2)  # стоимость обследовательских работ
         # round и "2" в конце - округляем до второго знака после запятой
         results_3 = round(results_1 + results_2, 2)
-
-        book.close()
 
         return render_template('smeta_pir.html',
                                the_volume=volume,
